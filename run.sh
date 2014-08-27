e593ec0bd172f637b6525ec05f73ab7452125a66
Naveen Narayanan <nnarayanan12@gmail.com>
2014-08-27 00:31:17 -0400

    Added check for directory permissions for npm and node

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
+echo "Checking if permission to access nodejs is given"
+perms==$(stat nodejs | sed -n '/^Access: (/{s/Access: (\([0-9]\+\).*$/\1/;p}')
+if [[ $perms =~ 755 ]]; then
+    echo "Need to chmod nodejs directory to update dependencies"
+    sudo chmod -R 755 nodejs
+fi
+
 echo "Running setup"
-nodejs/bin/node_modules/npm/bin/npm install -l
-nodejs/bin/node_modules/npm/bin/npm install -l node_modules/nodemon/
+nodejs/bin/node nodejs/bin/node_modules/npm/bin/npm-cli.js install -l
+nodejs/bin/node nodejs/bin/node_modules/npm/bin/npm-cli.js install -l node_modules/nodemon/
 
 echo "Starting app"
 nodejs/bin/node node_modules/nodemon/bin/nodemon.js -e json -w /config
