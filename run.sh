01c82594803e85ef57444ff40c0ee9f5f45999ce
Naveen Narayanan <nnarayanan12@gmail.com>
2014-08-27 00:50:30 -0400

    Always chmod nodejs directory

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
 #! /bin/bash
 
-echo "Checking if permission to access nodejs is given"
-
-perm=$(stat -c nodejs "$f")
-
-if [ "$perm" = "777" ]; then
-    echo "$f permissions are 777"
-else
-    echo "Need to chmod nodejs directory to update dependencies"
-    sudo chmod -R 777 nodejs
-fi
+echo "Need to chmod nodejs directory to update dependencies"
+sudo chmod -R 777 nodejs
 
 echo "Running setup"
 nodejs/bin/node nodejs/bin/node_modules/npm/bin/npm-cli.js install -l
